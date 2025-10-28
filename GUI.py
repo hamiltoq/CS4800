b7377d9082d21e46864e3ff69eb293c35bd11936
Quinn Hamilton <165169739+hamiltoq@users.noreply.github.com>
2025-10-28 16:25:27 -0500

    started gui

---
+++ GUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+window.geometry("420x420")
+window.title("Automated Archival Process")
+
+label = Label(window, text="Data Accessioner", font=("Arial", 15, "bold"))
+label.pack()
+
+window.mainloop()
