51c5f67fc647b0bd4a3812e75b7da3bd9cdffca6
Quinn Hamilton <165169739+hamiltoq@users.noreply.github.com>
2025-10-30 11:37:13 -0500

    added open file button to gui

--- GUI.py
+++ GUI.py
@@ -1,11 +1,46 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 
 window = Tk()
 window.geometry("420x420")
 window.title("Automated Archival Process")
 
-label = Label(window, text="Data Accessioner", font=("Arial", 15, "bold"))
-label.pack()
+labelDA = Label(window, text="Data Accessioner", font=("Arial", 15, "bold"))
+labelDA.pack()
+
+labelXSLT = Label(window, text="XSLT Processor", font=("Arial", 15, "bold"))
+labelXSLT.pack()
+
+labelDAF = Label(window, text="DAFixity", font=("Arial", 15, "bold"))
+labelDAF.pack()
+
+def select_file():
+    filetypes = (
+        ('All files', '*.*'),
+        ('Text files', '*.txt')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+
+
+open_button = ttk.Button(
+    window,
+    text='Choose File',
+    command=select_file
+)
+
+open_button.pack(expand=True)
+
 
 window.mainloop()
+
