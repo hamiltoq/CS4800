ad747b38342e1b4c58d24cd51918ab3463166fc7
Quinn Hamilton <165169739+hamiltoq@users.noreply.github.com>
2025-12-15 14:15:46 -0600

    changed dafixity to fixity

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from pathlib import Path
-from cs4800 import generate_data_accessioner_xml, run_xslt_processor, run_dafixity
+from cs4800 import generate_data_accessioner_xml, run_xslt_processor, run_fixity
 
 def run_pipeline():
     #gets all of the inputs from the GUI
@@ -30,8 +30,8 @@ def run_pipeline():
         run_xslt_processor(xml_report, xslt_csv, csv_transformed)
         run_xslt_processor(xml_report, xslt_html, html_transformed)
 
-        #DAFixity
-        fixity_csv, fixity_log = run_dafixity(xml_report, out_dir, accession_number, Path(data_dir))
+        #Fixity
+        fixity_csv, fixity_log = run_fixity(xml_report, out_dir, accession_number, Path(data_dir))
 
         #message box that shows up when the pipeline is complete
         messagebox.showinfo(
@@ -40,7 +40,7 @@ def run_pipeline():
             f"XML: {xml_report}\n\n"
             f"XSLT Processor complete!\n"
             f"CSV: {csv_transformed}\nHTML: {html_transformed}\n\n"
-            f"DAFixity complete!\n"
+            f"Fixity complete!\n"
             f"Fixity CSV: {fixity_csv}\nFixity Log: {fixity_log}"
         )
 
@@ -80,3 +80,4 @@ tk.Checkbutton(root, text="Move files instead of copy", variable=move_var).grid(
 tk.Button(root, text="Run Pipeline", command=run_pipeline, bg="#4CAF50", fg="white", width=15).grid(row=5, column=1, pady=20)
 
 root.mainloop()
+
